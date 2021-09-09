93cbe62db9f4d678c3e071c1d2e9f6b2031ee3b1
nttvy <thi-tuong-vy.nguyen@meduniwien.ac.at>
2021-09-09 10:22:27 +0200

    update description file and metadata file based on bioc reviews

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,19 +1,18 @@
 meta.data <- data.frame(Title = 'Pretrained models',
                         Description = 'Pretrained classification models by and for scAnnotatR package',
-                        BiocVersion = 3.13,
+                        BiocVersion = 3.14,
                         Genome = NA,
                         SourceType = 'RDA',
                         SourceUrl = 'https://github.com/grisslab/scAnnotatR-models/raw/main/default_models.rda',
                         SourceVersion = 'v1',
-                        Species = 'Homo Sapiens',
+                        Species = 'Homo sapiens',
                         TaxonomyId = 9606,
                         Coordinate_1_based = NA,
                         DataProvider = '',
                         Maintainer = 'Johannes Griss <johannes.griss@meduniwien.ac.at>',
                         RDataClass = 'list',
                         DispatchClass = 'Rda',
-                        Location_Prefix = 'https://github.com/grisslab/scAnnotatR-models/raw/main/',
-                        RDataPath = 'default_models.rda'
+                        RDataPath = 'scAnnotatR.models/default_models.rda'
 )
 
 write.csv(meta.data, file = 'inst/extdata/metadata.csv', row.names = FALSE)
